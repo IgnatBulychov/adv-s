74c1ed010170f17f94166570fe7a6562da74a471
IgnatBulychov <jose220593@gmail.com>
2022-04-26 16:07:23 +0300

    star migration

--- migrations/20220425154213_create_stars_table.js
+++ migrations/20220425154213_create_stars_table.js
@@ -1,15 +1,12 @@
 
 exports.up = function(knex) {
   return knex.schema
-  .createTable('offer_messages', function (table) {
+  .createTable('stars', function (table) {
     table.string('id').notNullable().primary();
-    table.string('text', 4096).notNullable();
+    table.string('authorId', 255).notNullable();
     table.string('offerId', 255).notNullable()
       .references('offers.id') // ...which references Article PK.
       .onDelete('CASCADE');
-    table.string('from', 255).notNullable();
-    table.string('to', 255).notNullable();
-    table.boolean('isViewed', 255);
     table.timestamp('createdAt').defaultTo(knex.fn.now());
     table.timestamp('updatedAt').defaultTo(knex.fn.now());
   })
@@ -17,5 +14,5 @@ exports.up = function(knex) {
 
 exports.down = function(knex) {
   return knex.schema
-  .dropTable("offer_messages");
+  .dropTable("stars");
 };
