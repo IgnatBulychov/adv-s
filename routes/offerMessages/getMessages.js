9818903e3ea8711da04caada0d31b69f8a9a0b6e
IgnatBulychov <jose220593@gmail.com>
2022-06-02 09:20:07 +0300

    fix mess

--- routes/offerMessages/getMessages.js
+++ routes/offerMessages/getMessages.js
@@ -2,7 +2,11 @@ const db = require('../../db/db');
 
 module.exports = async (ctx) => {
 
-  let messages = await db('offer_messages')
+  let messages = await db.select(['id', 'text', 'offerId', 'from' , 'to', 'isViewed'])
+  .from('offer_messages')
+  .where({
+    offerId: ctx.offer.id
+  })
 
   messages.map(message => {
     message.isFromMe = message.from == ctx.request.jwtPayload.data.sub
